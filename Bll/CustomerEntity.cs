9f295bcb92c3320becc87fd9b15e5a9de2e07bb0
Kingcean Tuan <kingcean@live.com>
2021-01-02 22:40:07 +0800

    Update customer entity.

--- Bll/CustomerEntity.cs
+++ Bll/CustomerEntity.cs
@@ -49,6 +49,17 @@ namespace NuScien.Sample
             set => SetCurrentProperty(value);
         }
 
+        /// <summary>
+        /// Gets or sets the identifier of the owner site.
+        /// </summary>
+        [JsonPropertyName("site")]
+        [Column("site")]
+        public string SiteId
+        {
+            get => GetCurrentProperty<string>();
+            set => SetCurrentProperty(value);
+        }
+
         #endregion
 
         #region Member methods
@@ -56,6 +67,19 @@ namespace NuScien.Sample
         #endregion
 
         #region Static methods
+
+        /// <summary>
+        /// Searches.
+        /// </summary>
+        /// <param name="name">The name to search.</param>
+        /// <param name="like">true if search by like; otherwise, false, to equals to.</param>
+        /// <param name="state">The state.</param>
+        /// <returns>A collection.</returns>
+        public static IEnumerable<CustomerEntity> Search(string name, bool like = false, ResourceEntityStates state = ResourceEntityStates.Normal)
+        {
+            var context = Internals.BusinessDbContext.Create(true);
+            return context.Customers.ListEntities(name, like, state);
+        }
 
         /// <summary>
         /// Searches.
@@ -85,10 +109,24 @@ namespace NuScien.Sample
         /// <param name="value">The entity to add or update.</param>
         /// <param name="cancellationToken">The optional token to monitor for cancellation requests.</param>
         /// <returns>The change method.</returns>
-        public static Task<ChangeMethods> SaveAsync(CustomerEntity value, CancellationToken cancellationToken = default)
+        /// <example>
+        /// var client = ResourceAccessClients.CreateAsync();
+        /// // client.LoginAsync(...);
+        /// var entity = CustomerEntity.GetById("abcdefg") ?? new CustomerEntity
+        /// {
+        ///     Name = "abcdefg",
+        ///     State = ResourceEntityStates.Normal
+        /// };
+        /// await CustomerEntity.SaveAsync(client, entity);
+        /// </example>
+        public static async Task<ChangeMethods> SaveAsync(BaseResourceAccessClient client, CustomerEntity value, CancellationToken cancellationToken = default)
         {
+            if (value.IsNew
+                ? string.IsNullOrWhiteSpace(value.SiteId)   // Property check for new entity.
+                : !await client.HasPermissionAsync(value.SiteId, "sample-customer-management")) // Permission check.
+                return ChangeMethods.Invalid;
             var context = Internals.BusinessDbContext.Create(false);
-            return DbResourceEntityExtensions.SaveAsync(context.Customers, context.SaveChangesAsync, value, cancellationToken);
+            return await DbResourceEntityExtensions.SaveAsync(context.Customers, context.SaveChangesAsync, value, cancellationToken);
         }
 
         #endregion
